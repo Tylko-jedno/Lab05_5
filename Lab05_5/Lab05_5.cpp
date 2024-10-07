1c495c1d6cdfa895e0ef4f11f3750139c964fc4f
Tylko-jedno <diogkek@gmail.com>
2024-10-07 19:46:04 +0300

    first master commit

---
+++ Lab05_5/Lab05_5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int f(const int n, int& depth) {
+    if (n == 0)
+        return 0;
+    else {
+        depth += 1;
+        return 1 + f(n & (n - 1), depth);
+    } 
+}
+
+int main()
+{
+    int n;
+    int depth = 0;
+    cout << "n = "; cin >> n;
+    cout << f(n, depth) << " " << depth;
+}
+
+
+
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab05_5/Lab05_5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int depth = 0;
+			int t = f(10, depth);
+			double expected = 2;
+			double epsilon = 0.0001;   // Точність порівняння
+			Assert::AreEqual(expected, t, epsilon);
+		}
+	};
+}
